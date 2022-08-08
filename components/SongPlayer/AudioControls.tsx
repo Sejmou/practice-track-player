42ed7e756b4a0b305761cd3da4379b8af2e64cda
Sejmou <samo.kolter@gmail.com>
2022-08-08 13:13:22 +0200

    control song playback with space key

--- components/SongPlayer/AudioControls.tsx
+++ components/SongPlayer/AudioControls.tsx
@@ -1,10 +1,41 @@
+import { useEffect, useRef, useState } from 'react';
+
 import { SourceData } from '@models';
+import {
+  KeyboardShortcuts,
+  KeyboardShortcut,
+} from '@frontend/keyboard-shortcuts';
+
+const shortcuts = new KeyboardShortcuts();
 
 type Props = { audioData: SourceData };
 const AudioControls = ({ audioData }: Props) => {
+  const audioElRef = useRef<HTMLAudioElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    shortcuts.set([
+      new KeyboardShortcut({ code: 'Space' }, () => {
+        setIsPlaying(previous => !previous);
+      }),
+    ]);
+  }, []);
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioElRef.current?.play();
+    } else {
+      audioElRef.current?.pause();
+    }
+  }, [isPlaying]);
+
   const { src, type } = audioData;
   return (
-    <audio controls>
+    <audio
+      ref={audioElRef}
+      onKeyDownCapture={keyboardEvent => shortcuts.applyMatching(keyboardEvent)}
+      controls
+    >
       <source src={src} type={type} />
     </audio>
   );
