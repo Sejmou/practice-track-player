657707d229b3bcf860e6c2956eeb8b93b6cf620e
Sejmou <samo.kolter@gmail.com>
2022-08-13 19:01:42 +0200

    fix TS error preventing build

--- pages/api/musicals/[musicalId]/index.ts
+++ pages/api/musicals/[musicalId]/index.ts
@@ -2,7 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { Musical } from '@models';
-import { getMusical } from 'musicals';
+import { getMusical } from '@backend/musical-data';
 
 export default async function handler(
   req: NextApiRequest,
