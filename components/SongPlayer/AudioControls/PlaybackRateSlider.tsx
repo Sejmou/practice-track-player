0d6f0a44a717b944f9ed219851c409c1b7afbf16
Sejmou <samo.kolter@gmail.com>
2022-08-14 16:43:48 +0200

    implement keyboard shortcut support :)

--- components/SongPlayer/AudioControls/PlaybackRateSlider.tsx
+++ components/SongPlayer/AudioControls/PlaybackRateSlider.tsx
@@ -1,42 +1,46 @@
 import { Box, Grid, Slider, Stack, SxProps, Typography } from '@mui/material';
 // import SlowMotionVideoIcon from '@mui/icons-material/SlowMotionVideo';
 import SpeedIcon from '@mui/icons-material/Speed';
+import React from 'react';
 
 type Props = {
   playbackRate: number;
+  min: number;
+  max: number;
   onPlaybackRateChange: (pbr: number) => void;
   sx?: SxProps;
 };
-const PlaybackRateSlider = ({
-  playbackRate,
-  onPlaybackRateChange,
-  sx,
-}: Props) => {
-  const handleSliderChange = (event: Event, newValue: number | number[]) => {
-    onPlaybackRateChange(newValue as number);
-  };
+const PlaybackRateSlider = React.forwardRef<HTMLInputElement, Props>(
+  ({ playbackRate, onPlaybackRateChange, sx, min, max }: Props, ref) => {
+    const handleSliderChange = (event: Event, newValue: number | number[]) => {
+      onPlaybackRateChange(newValue as number);
+    };
 
-  return (
-    <Stack
-      width="100%"
-      spacing={1}
-      direction="row"
-      sx={{ p: '16px', ...sx }}
-      alignItems="center"
-    >
-      <SpeedIcon color="primary" />
-      <Typography>{playbackRate.toFixed(2)}x</Typography>
-      <Slider
-        sx={{ ml: '16px !important' }}
-        value={playbackRate}
-        min={0.5}
-        max={1}
-        step={0.05}
-        size="small"
-        onChange={handleSliderChange}
-        aria-labelledby="input-slider"
-      />
-    </Stack>
-  );
-};
+    return (
+      <Stack
+        width="100%"
+        spacing={1}
+        direction="row"
+        sx={{ p: '16px', ...sx }}
+        alignItems="center"
+      >
+        <SpeedIcon color="primary" />
+        <Typography>{playbackRate.toFixed(2)}x</Typography>
+        <Slider
+          ref={ref}
+          sx={{ ml: '16px !important' }}
+          value={playbackRate}
+          min={min}
+          max={max}
+          step={0.05}
+          size="small"
+          onChange={handleSliderChange}
+          aria-labelledby="input-slider"
+        />
+      </Stack>
+    );
+  }
+);
+PlaybackRateSlider.displayName = 'PlaybackRateSlider';
+
 export default PlaybackRateSlider;
