3e5fcf20403c9fcf8867254a2257b70807dca610
Sejmou <samo.kolter@gmail.com>
2022-09-08 17:36:25 +0200

    make track filter responsive

--- frontend/musical/TrackFilter.tsx
+++ frontend/musical/TrackFilter.tsx
@@ -3,6 +3,7 @@ import {
   Box,
   Button,
   Chip,
+  Grid,
   ListItem,
   Paper,
   Stack,
@@ -21,11 +22,25 @@ const TrackFilter = () => {
   } = useMusicalContext();
 
   return (
-    <Stack direction="row" component={Paper}>
+    <Paper
+      sx={{
+        display: 'grid',
+        p: 1,
+        gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 128px',
+        gridTemplateAreas: {
+          xs: '"chips chips chips chips chips chips" "autocomplete autocomplete autocomplete autocomplete autocomplete buttons"',
+          md: '"autocomplete chips chips chips chips buttons"',
+        },
+        alignItems: 'center',
+      }}
+    >
       <Autocomplete
         disablePortal
         size="small"
-        sx={{ width: 300, p: 1 }}
+        sx={{
+          width: '100%',
+          gridArea: 'autocomplete',
+        }}
         renderInput={params => <TextField {...params} label="Filter tracks" />}
         options={trackFilterOptions.filter(
           o => !stagedTrackFilters.find(a => o.label === a.label)
@@ -37,11 +52,37 @@ const TrackFilter = () => {
           }
         }}
       />
-      <Stack p={0} m={0} component="ul" direction="row">
+      {stagedTrackFilters.length > 0 && (
+        <Box
+          sx={{
+            gridArea: 'buttons',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'row',
+          }}
+        >
+          <Box alignSelf="center">
+            <Button onClick={applyFilters}>Apply</Button>
+          </Box>
+          <Box alignSelf="center">
+            <Button onClick={resetFilters}>Reset</Button>
+          </Box>
+        </Box>
+      )}
+      <Stack
+        px={0.5}
+        m={0}
+        sx={{ gridArea: 'chips' }}
+        component="ul"
+        direction="row"
+        overflow="auto"
+      >
         {stagedTrackFilters.map((f, i) => (
-          <ListItem sx={{ p: 1 }} key={i}>
+          <ListItem sx={{ py: 0, px: 0.5, width: 'min-content' }} key={i}>
             <Chip
               label={f.label}
+              sx={{ mb: { xs: 2, md: 0 } }}
               onDelete={() => {
                 removeTrackFilter(f);
                 if (stagedTrackFilters.length === 1) resetFilters();
@@ -50,17 +91,7 @@ const TrackFilter = () => {
           </ListItem>
         ))}
       </Stack>
-      {stagedTrackFilters.length > 0 && (
-        <>
-          <Box alignSelf="center">
-            <Button onClick={applyFilters}>Apply</Button>
-          </Box>
-          <Box alignSelf="center">
-            <Button onClick={resetFilters}>Reset</Button>
-          </Box>
-        </>
-      )}
-    </Stack>
+    </Paper>
   );
 };
 export default TrackFilter;
